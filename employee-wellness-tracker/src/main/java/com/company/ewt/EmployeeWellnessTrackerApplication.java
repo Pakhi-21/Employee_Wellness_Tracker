4d37fb2aed38534ee2e30a1a57db1e40ab4003e2
Pakhi-21 <pakhigupta556@gmail.com>
2025-03-28 09:20:50 +0530

    Initial commit: Project setup

---
+++ employee-wellness-tracker/src/main/java/com/company/ewt/EmployeeWellnessTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.company.ewt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeWellnessTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeWellnessTrackerApplication.class, args);
+	}
+
+}
