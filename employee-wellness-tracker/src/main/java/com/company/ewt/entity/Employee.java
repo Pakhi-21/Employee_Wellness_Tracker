1ee94d507d2b92d4f4a3374de5b01eb3396546ee
Pakhi-21 <pakhigupta556@gmail.com>
2025-03-30 18:17:31 +0530

    update employee entity

--- employee-wellness-tracker/src/main/java/com/company/ewt/entity/Employee.java
+++ employee-wellness-tracker/src/main/java/com/company/ewt/entity/Employee.java
@@ -1,10 +1,17 @@
 package com.company.ewt.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 import lombok.*;
@@ -34,5 +41,11 @@ public class Employee {
     private String location;
 
     private boolean isAdmin = false; 
+
+
+    @OneToMany(mappedBy = "employee", cascade = CascadeType.ALL, orphanRemoval = true)
+    @JsonManagedReference 
+    private List<SurveyResponse> surveyResponses = new ArrayList<>();
+
 
 } 
